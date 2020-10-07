cbf8d528c05200a5b4ed0852265ceb5e94189047
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-10-06 20:33:50 -0400

    used moment.js to display the current date and time.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("hello")
-console.log(this)
+// Global variables that will represent rows in the calender
+var timeDiv;
+var entryDiv;
+var saveBtnDiv;
+
+// Display the current date and time
+var momentDisplay = moment().format('MMM Do YYYY, h:mm:a');
+document.getElementById("currentDay").innerHTML = momentDisplay;
