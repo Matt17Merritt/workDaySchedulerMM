907041a80334ab63004ce826dbd0c3a045ed6dc5
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-10-06 22:25:08 -0400

    added an array to track time of day hours and a loop to create => append to div in html.

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ var saveBtnDiv;
 // Display the current date and time
 var momentDisplay = moment().format('MMM Do YYYY, h:mm:a');
 document.getElementById("currentDay").innerHTML = momentDisplay;
+
+// console.log(Intl.DateTimeFormat)
+
+// Array of time of day indicators and numbers
+var timeSlots = ["8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm",];
+var workHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+for (var i = 0; i < timeSlots.length;i++) {
+    let timeDiv = $("<p>");
+    timeDiv.addClass("hour row");
+    timeDiv.text(timeSlots[i]);
+    $(".float-left").append(timeDiv);
+}
