4aae3615f1f935dfe17dcdd40fe8a996821608b3
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-10-07 16:00:28 -0400

    added a div to hold the input for the calender

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ document.getElementById("currentDay").innerHTML = momentDisplay;
 // console.log(Intl.DateTimeFormat)
 
 // Array of time of day indicators and numbers
-var timeSlots = ["8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm",];
-var workHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+var timeSlots = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm",];
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+// For loop adding timeSlots to timeDiv
 for (var i = 0; i < timeSlots.length;i++) {
     let timeDiv = $("<p>");
     timeDiv.addClass("hour row");
     timeDiv.text(timeSlots[i]);
     $(".float-left").append(timeDiv);
 }
+
+// 
+
